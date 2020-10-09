3aa1e49eaa9afa51b098cfe74601cd06001156d6
Nicolás Cervera <nicodapa@live.com>
2020-10-09 01:23:11 -0300

    api corriendo BD Conectada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ProductosComponent } from './producto/productos/productos.component';
+// import { ProductosComponent } from './producto/productos/productos.component';
 import { ProductoComponent } from './producto/producto/producto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductosComponent,
+    /*ProductosComponent*/,
     ProductoComponent
   ],
   imports: [
